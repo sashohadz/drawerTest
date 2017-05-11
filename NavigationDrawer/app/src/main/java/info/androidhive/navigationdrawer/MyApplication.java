cfba2c7bc41a350edf5d4f09bcf306758403af4e
shadzhiev <sasho.hadzhiev@yahoo.com>
2017-05-11 12:32:18 -0700

    StrictMode addition

--- NavigationDrawer/app/src/main/java/info/androidhive/navigationdrawer/MyApplication.java
+++ NavigationDrawer/app/src/main/java/info/androidhive/navigationdrawer/MyApplication.java
@@ -1,6 +1,7 @@
 package info.androidhive.navigationdrawer;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import com.leanplum.Leanplum;
 import com.leanplum.LeanplumActivityHelper;
@@ -13,16 +14,30 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()   // or .detectAll() for all detectable problems
+                .penaltyLog()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build());
+
         Leanplum.setApplicationContext(this);
         LeanplumActivityHelper.enableLifecycleCallbacks(this);
 
 
         if (BuildConfig.DEBUG) {
             System.out.println("DEV MODE");
-            Leanplum.setAppIdForDevelopmentMode("app_", "dev_");
+            Leanplum.setAppIdForDevelopmentMode("app_nQGF1lrbStJopxK41pzNwueB7jBbWp4Hl0Qqq1BsYNo", "dev_5yE7VOobZuvUrgoTouFDM7CvZTvD3gDeoze0VNqetTo");
         } else {
-            System.out.println("PROD MODE");
-            Leanplum.setAppIdForProductionMode("app_", "prod_");
+        System.out.println("PROD MODE");
+        Leanplum.setAppIdForProductionMode("app_nQGF1lrbStJopxK41pzNwueB7jBbWp4Hl0Qqq1BsYNo", "prod_a6cwir5070QOAaVmtkxRAgTD88J40rLWy9lSkvBfJk0");
         }
 
         Leanplum.enableVerboseLoggingInDevelopmentMode();
