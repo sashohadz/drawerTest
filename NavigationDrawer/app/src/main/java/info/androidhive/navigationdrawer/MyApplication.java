b6dbfdf9c21c6d6641c0f704d4b449135676b130
shadzhiev <sasho.hadzhiev@yahoo.com>
2017-05-22 12:31:44 +0300

    lp sdk version update to 2.2.2.

--- NavigationDrawer/app/src/main/java/info/androidhive/navigationdrawer/MyApplication.java
+++ NavigationDrawer/app/src/main/java/info/androidhive/navigationdrawer/MyApplication.java
@@ -15,18 +15,18 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-                .detectDiskReads()
-                .detectDiskWrites()
-                .detectNetwork()   // or .detectAll() for all detectable problems
-                .penaltyLog()
-                .build());
-        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
-                .detectLeakedSqlLiteObjects()
-                .detectLeakedClosableObjects()
-                .penaltyLog()
-                .penaltyDeath()
-                .build());
+//        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+//                .detectDiskReads()
+//                .detectDiskWrites()
+//                .detectNetwork()   // or .detectAll() for all detectable problems
+//                .penaltyLog()
+//                .build());
+//        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+//                .detectLeakedSqlLiteObjects()
+//                .detectLeakedClosableObjects()
+//                .penaltyLog()
+//                .penaltyDeath()
+//                .build());
 
         Leanplum.setApplicationContext(this);
         LeanplumActivityHelper.enableLifecycleCallbacks(this);
